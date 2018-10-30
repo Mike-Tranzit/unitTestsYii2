281b9ad22d16946d08ec006226e3155108dfa199
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2018-10-30 18:47:50 +0300

    commit 75

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -90,6 +90,7 @@ class SiteController extends Controller
                                 // Тестовый комментарий fix 68
                                 // Тестовый комментарий fix 69
                                 // Тестовый комментарий fix 74
+                                // Тестовый комментарий fix 75
         if($model->load(Yii::$app->request->post())) {
             //var_dump(\Yii::$app->request->post());
             //var_dump($model);
